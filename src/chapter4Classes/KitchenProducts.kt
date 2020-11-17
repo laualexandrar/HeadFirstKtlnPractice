40eb0fd3cd8b4d04d085a1d65ac90cea96b46808
Laura Roa <laualexandrar@gmail.com>
2020-11-16 22:57:33 -0800

    Chapter 4 practice 2

---
+++ src/chapter4Classes/KitchenProducts.kt
@@ -0,0 +1,26 @@
+package chapter4Classes
+
+class KitchenProducts (val name: String, val category: String, val quantity: Int,  price_param: Int, taste_param: String){
+
+    val taste = taste_param
+    var price = price_param
+    set(value) {
+        if (value > 0 ) field = value
+    }
+
+    val priceInColombian: Int
+    get() = price * 3600
+
+
+}
+
+fun main(){
+
+    val myKitchenProducts = KitchenProducts ("Panela", "Sugar", 2, 3, "Sweet")
+
+    println ("my kitchen is full of ${myKitchenProducts.name}, I bought ${myKitchenProducts.quantity} " +
+            "of them and I found them in the ${myKitchenProducts.category} category")
+
+    println("in Colombian Pesos the ${myKitchenProducts.name} is ${myKitchenProducts.priceInColombian} ")
+
+}
