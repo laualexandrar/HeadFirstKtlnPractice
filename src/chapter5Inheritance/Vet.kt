c7c6d13cbb54ca7c82cc6a5b17ae17c23eedaeb5
Laura Roa <laualexandrar@gmail.com>
2020-11-20 00:53:20 -0800

    Changes

--- src/chapter5Inheritance/Vet.kt
+++ src/chapter5Inheritance/Vet.kt
@@ -9,19 +9,3 @@ class Vet {
     }
 }
 
-fun main (){
-
-    val animals = arrayOf(Hippo(),Wolf())
-    for (item in animals){
-        item.roam()
-        item.eat()
-    }
-
-    val vet = Vet()
-    val wolf = Wolf()
-    val hippo = Hippo()
-
-    vet.giveShot(wolf)
-    vet.giveShot(hippo)
-
-}
