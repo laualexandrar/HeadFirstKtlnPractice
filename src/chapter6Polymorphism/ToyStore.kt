cf6b3ac89ff2a516eb2f7589999f9635ad0a78ae
Laura Roa <laualexandrar@gmail.com>
2020-11-21 18:40:07 -0800

    Practicing1

---
+++ src/chapter6Polymorphism/ToyStore.kt
@@ -0,0 +1,18 @@
+package chapter6Polymorphism
+
+abstract class ToyStore {
+
+    abstract  val department: String
+    abstract val NumberOfEmployees: Int
+    abstract val typeOfToys: String
+
+    abstract fun 
+
+}
+
+fun main(){
+
+    val myToyStore = arrayOf("Kids Toys", "Strollers", "Baby Toys", "Blankets")
+
+    myToyStore
+}
