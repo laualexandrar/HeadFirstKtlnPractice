960176d35324f51aa852b369644a10d6e04fcfd5
Laura Roa <laualexandrar@gmail.com>
2020-11-23 23:33:52 -0800

    Nulls

---
+++ src/chapter8Null/Cat.kt
@@ -0,0 +1,17 @@
+package chapter8Null
+
+class Cat (var name:String? = ""){
+    fun Meow(){
+        println("Meow!")
+    }
+}
+
+fun main(){
+    var myCats = arrayOf(Cat("Misty"), null, Cat("Socks"))
+    for(cat in myCats){
+        if(cat!=null){
+            print("${cat.name}: ")
+            cat.Meow()
+        }
+    }
+}
