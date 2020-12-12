9deea30d311f92345d9a98977f51f67f24cdf41d
Laura Roa <laualexandrar@gmail.com>
2020-12-12 00:39:46 -0800

    Learning Maps

--- src/chapter9Collections/Collections.kt
+++ src/chapter9Collections/Collections.kt
@@ -1,5 +1,7 @@
 package chapter9Collections
 
+data class Recipe (var name : String)
+
 fun main() {
     var mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
     println("mShoppingList original: $mShoppingList")
@@ -22,4 +24,18 @@ fun main() {
     println("mShoppingSet items added: $mShoppingSet")
     mShoppingList = mShoppingSet.toMutableList()
     println("mShoppingList new version: $mShoppingList")
+
+    val r1 = Recipe("Chicken Soup")
+    val r2 = Recipe ("Quinoa Salad")
+    val r3 = Recipe("Thai Curry")
+    val r4 = Recipe("Jambalaya")
+    val r5 = Recipe("SausageRolls")
+    val mRecipeMap = mutableMapOf("Recipe1" to r1, "Recipe2" to r2, "Recipe3" to r3)
+    println("mRecipeMap original: $mRecipeMap")
+    val recipesToAdd = mapOf("Recipe4" to r4, "Recipe5" to r5)
+    mRecipeMap.putAll(recipesToAdd)
+    println("mRecipeMap updated: $mRecipeMap")
+    if (mRecipeMap.containsKey("Recipe1")){
+        println("Recipe1 is: ${mRecipeMap.getValue("Recipe1")}")
+    }
 }
