561a69f73d1a974a7da53e6d6568e0888ab37773
Laura Roa <laualexandrar@gmail.com>
2021-01-18 23:07:55 -0800

    practicing lambdas

---
+++ src/chapter11Lambdas/MixedMessages11.kt
@@ -0,0 +1,21 @@
+package chapter11Lambdas
+
+fun main() {
+    val x = 20
+    val y = 2.3
+    val lam1 = {x : Int -> x}
+    println(lam1(x + 6))
+
+    val lam2: (Double) -> Double
+    lam2 = { (it * 2) + 5}
+    println(lam2(y))
+
+    val lamb3: (Double, Double) -> Unit
+    lamb3 = {x, y -> println(x + y)} // ????  it is ignoring this?
+    lamb3.invoke(y,y)
+
+    var lam4 = {y: Int -> (y/2).toDouble()}
+    print(lam4(x))
+    lam4 = { it + 6.3 }
+    println(lam4(7))
+}
